46150f6bf35f03c527c2eb83fdf6ef4a5987772f
Mikhail.H <m.haidayenka@yandex.by>
2019-05-14 18:04:20 +0300

    add new file in pascal

---
+++ Pascal/hesh_otkritiy.pas
@@ -0,0 +1,22 @@
+﻿program hesh1;
+var
+  i,B:integer;
+  S:string[20];
+  
+  function h(x:string[20];j:integer):integer;
+    var i,sum:integer;
+    begin
+      sum:=0;
+      for i:=1 to j do 
+      sum:=sum+ord(x[i]);
+      h:=sum mod B;
+    end;
+    
+begin
+  writeln('введите B');
+  readln(B);
+  writeln('введите ключ');
+  readln(S);
+  i:=length(S);
+  writeln(h(s,i));
+end.
